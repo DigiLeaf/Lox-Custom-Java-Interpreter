4e409d891f9c7c8ba8ca0c0bcc69168ff440826b
DigiLeaf <leiforth01@hotmail.com>
2026-05-14 22:01:28 -0500

    Add token class, basic error handling, and run functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
  # Lox-Custom-Java-Interpreter
+This is a custom interpreter for the Lox language I built in Java.
+Lox is a language created in the book Crafting Interpreters by Robert Nystrom.
--- src/main/DigiLeafprojects/lox/Lox.java
+++ src/main/DigiLeafprojects/lox/Lox.java
@@ -1,5 +1,6 @@
 package main.DigiLeafprojects.lox;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,8 +8,11 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Lox {
+
+  static boolean hadError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -19,4 +23,44 @@ public class Lox {
       runPrompt();
     }
   }
+  private static void runFile(String path) throws IOException{
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    if (hadError) System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException{
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String, message){
+    report(line, "", message);
+  }
+
+  private static void report( int line, String where, String message){
+    System.err.println("[line " + line + "] Error" + where +": " + message);
+
+    hadError = true;
+  }
 }
+
